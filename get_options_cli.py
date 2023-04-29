cdb8d45e54e4394100479f1876e137b4641db369
13lv <13lv@example.com>
2023-04-29 10:17:09 +0600

    change file

--- get_options_cli.py
+++ get_options_cli.py
@@ -23,19 +23,19 @@ def _get_options():
     allow_opts_values = {
 #       <short option>: (<variable name>, <regex option value>)
 
-        '-a': ('a_var', r'^[A-z]{10,255}$'),
-        '--along': ('along_var', r'^[A-z]{10,255}$'),
-        '-b': ('b_var', r'^[A-z]{10,255}$'),
-        '--blong': ('blong_var', r'^[A-z]{10,255}$'),
-        '-c': ('c_var', r'^[A-z]{10,255}$'),
-        '--clong': ('clong_var', r'^[A-z]{10,255}$'),
+        '-a': ('a_var', r'^[A-z]{3,255}$'),
+        '--along': ('along_var', r'^[A-z]{7,255}$'),
+        '-b': ('b_var', r'^[A-z]{3,255}$'),
+        '--blong': ('blong_var', r'^[A-z]{7,255}$'),
+        '-c': ('c_var', r'^[A-z]{3,255}$'),
+        '--clong': ('clong_var', r'^[A-z]{7,255}$'),
 
-        '-d': ('d_var', r'^[A-z]{10,255}$'),
-        '--dlong': ('dlong_var', r'^[A-z]{10,255}$'),
-        '-e': ('e_var', r'^[A-z]{10,255}$'),
-        '--elong': ('elong_var', r'^[A-z]{10,255}$'),
-        '-f': ('f_var', r'^[A-z]{10,255}$'),
-        '--flong': ('flong_var', r'^[A-z]{10,255}$'),
+        '-d': ('d_var', r'^[A-z]{3,255}$'),
+        '--dlong': ('dlong_var', r'^[A-z]{7,255}$'),
+        '-e': ('e_var', r'^[A-z]{3,255}$'),
+        '--elong': ('elong_var', r'^[A-z]{7,255}$'),
+        '-f': ('f_var', r'^[A-z]{3,255}$'),
+        '--flong': ('flong_var', r'^[A-z]{7,255}$'),
     }
 
     allow_opts = {
@@ -49,6 +49,7 @@ def _get_options():
     )
 
     def read_opt_arg(param, param_long):
+        print(param, param_long) # Test
         try:
             opts, args = getopt.getopt(sys.argv[1:], param, param_long)
         except getopt.GetoptError as e:
@@ -59,7 +60,7 @@ def _get_options():
             return opts, args
 
     for k, v in allow_opts.items():
-        print(k,v)
+        print(k,v) # Test
         opts, args = read_opt_arg(v[0], v[1])
 
         if not opts and not args:
@@ -76,10 +77,18 @@ def _get_options():
             help.help()
             sys.exit(1)
 
+    def chk_arg(opt='', val=''):
+        ptrn_arg = re.compile(allow_opts_values.get(opt, ())[1])
+        if ptrn_arg.match(val) is None:
+            _log.error(f"Option: \"{opt}\" has No Acceptable value \"{val}\"!")
+            help.help()
+            sys.exit(1)
+
     for o, ov in opts:
         _log.info(f"Option: \"{o}\", Option Value: \"{ov}\","
                   f" Options: \"{opts}\"")
 
+        chk_arg(o, ov)
         _cfg.OPTIONS[allow_opts_values.get(o)[0]] = ov
 
     return _cfg.OPTIONS
