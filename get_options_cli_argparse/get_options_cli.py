5dd6dec4e350e9708b41f556645dbce151dcaa86
13lv <13lv@example.com>
2023-04-30 21:21:59 +0600

    change file

--- get_options_cli_argparse/get_options_cli.py
+++ get_options_cli_argparse/get_options_cli.py
@@ -4,26 +4,49 @@
 import argparse
 # --- local modules ---
 
+
+parser = argparse.ArgumentParser(prog='ast-cdr-mgmt', 
+                description='*** <Description for Programm \"%(prog)s\"> ***',
+                epilog= 'Thanks for using \"%(prog)s\"! :)')
+
+parser.add_argument('arg_1', type=str, default='arg_arg_1', 
+        help='Test help arg_1')
+
+parser.add_argument('-a', '--along', dest='a_var', nargs=2, metavar=('A', 'B'), 
+        required=True, type=str, default="<opt_a_dflt>", 
+        help='Test help a \"%(metavar)s\"')
+
+parser.add_argument('-b', '--blong', dest='b_var', metavar='', type=str, 
+        choices=['b1', 'b2'], default="<opt_b_dflt>", help='Test help b')
+
+parser.add_argument('-c', '--clong', dest='c_var', metavar='', type=str, 
+            default="<opt_c_dflt>", help='Test help c')
+
+args = parser.parse_args()
+
+print(args)
+
+
 def get_options():
     """
     function input parameters:
     function output parameters:
     """
     parser = argparse.ArgumentParser(prog='ast-cdr-mgmt', 
-                description='*** <Description for Programm \"%(prog)S\"> ***',
+                description='*** <Description for Programm \"%(prog)s\"> ***',
                 epilog= 'Thanks for using \"%(prog)s\"! :)')
 
     parser.add_argument('arg_1', type=str, 
             default='arg_arg_1', help='Test help arg_1')
 
-    parser.add_argument('-a', '--along', nargs=2, metavar=('A', 'B'), 
+    parser.add_argument('-a', '--along', dest='a_var', nargs=2, metavar=('A', 'B'), 
     required=True, type=str, default="<opt_a_dflt>", 
     help='Test help a \"%(metavar)s\"')
 
-    parser.add_argument('-b', '--blong', metavar='', type=str, 
+    parser.add_argument('-b', '--blong', dest='b_var', metavar='', type=str, 
         choices=['b1', 'b2'], default="<opt_b_dflt>", help='Test help b')
 
-    parser.add_argument('-c', '--clong', metavar='', type=str, 
+    parser.add_argument('-c', '--clong', dest='c_var', metavar='', type=str, 
             default="<opt_c_dflt>", help='Test help c')
 
     args = parser.parse_args()
